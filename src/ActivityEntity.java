a65e581e3d16d5a2ef940e0d3a40156072342243
[TheGabrielRodriguez] <grodri70@calpoly.edu>
2021-11-03 14:53:24 -0700

    Project 3 updates, moved everything according to UML, need to fix current bugs where entities arent doing anything. Something in scheduleactions?

--- src/ActivityEntity.java
+++ src/ActivityEntity.java
@@ -14,8 +14,9 @@ public abstract class ActivityEntity extends AnimateEntity {
     public void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore){
         super.scheduleActions(scheduler,world, imageStore);
         scheduler.scheduleEvent(this,
-                Factory.createAnimationAction(this, 0),
-                getAnimationPeriod());
+                Factory.createActivityAction(this, world, imageStore),
+                this.getActionPeriod());
+
     }
     public int getActionPeriod(){return actionPeriod; }
     abstract void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler);
