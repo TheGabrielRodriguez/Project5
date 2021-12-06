9b4d50a78a41208a7c6f0c14ebfd7d4f9bf664c1
Jmulson <jmulson@calpoly.edu>
2021-12-06 13:04:02 -0800

    Jason

---
+++ src/CreateEntity.java
@@ -0,0 +1,13 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public abstract class CreateEntity extends RobustEntity{
+    public CreateEntity(String id, Point position, List<PImage> images, int animationPeriod, int actionPeriod) {
+        super(id, position, images, animationPeriod, actionPeriod);
+    }
+
+//    public void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore){
+//        super.scheduleActions(scheduler,world, imageStore);
+//    }
+}
