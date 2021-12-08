a61b28418a1320a4a26da05d4af751e1aeb1e176
Jmulson <jmulson@calpoly.edu>
2021-12-07 20:13:42 -0800

    Jason

--- src/CreateEntity.java
+++ src/CreateEntity.java
@@ -6,8 +6,4 @@ public abstract class CreateEntity extends RobustEntity{
     public CreateEntity(String id, Point position, List<PImage> images, int animationPeriod, int actionPeriod) {
         super(id, position, images, animationPeriod, actionPeriod);
     }
-
-    public void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore){
-        super.scheduleActions(scheduler,world, imageStore);
-    }
 }
