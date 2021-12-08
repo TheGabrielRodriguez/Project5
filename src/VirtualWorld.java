760b4fd281fd7de8b7686329ee53e2be9693d550
Marco <maraiza20026@gmail.com>
2021-12-08 08:35:48 -0800

    GIF to PNG

--- src/VirtualWorld.java
+++ src/VirtualWorld.java
@@ -81,7 +81,6 @@ public final class VirtualWorld extends PApplet
     // Just for debugging and for P5
     public void mousePressed() {
         Point pressed = mouseToPoint(mouseX, mouseY);
-        System.out.println("CLICK! " + pressed.getX() + ", " + pressed.getY());
 
         Optional<Entity> entityOptional = world.getOccupant(pressed);
 
